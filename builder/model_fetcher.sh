f3b2b26141ea936e8ff0e217754c1f951974b3a7
Tshwanne <96071569+Tshwanne@users.noreply.github.com>
2024-10-16 13:48:26 +0200

    Update model_fetcher.sh
    changed the script to download the model from Taa97/ZeroCool94_SDv1-5 rather than from  runwayml/stable-diffusion-v1-5

--- builder/model_fetcher.sh
+++ builder/model_fetcher.sh
@@ -2,7 +2,7 @@ mkdir /workspace/stable-diffusion-v1-5
 cd /workspace/stable-diffusion-v1-5
 git init
 git lfs install --system --skip-repo
-git remote add -f origin  "https://huggingface.co/runwayml/stable-diffusion-v1-5"
+# git remote add -f origin  "https://huggingface.co/Taa97/ZeroCool94_SDv1-5"
 git config core.sparsecheckout true
 echo -e "\nscheduler\ntext_encoder\ntokenizer\nunet\nvae\nmodel_index.json\n!vae/diffusion_pytorch_model.bin\n!*.safetensors" > .git/info/sparse-checkout
 git pull origin main
